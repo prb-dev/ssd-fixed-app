df6f2681ea3349d7e37f071bca45dba955a1b155
it21288630 <prabashwara.in@gmail.com>
2024-09-25 09:16:35 +0530

    Add csrf token to the request

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,4 @@
 <script type="module">
-    // Import the functions you need from the SDKs you need
     import {
         initializeApp
     } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
@@ -8,10 +7,8 @@
         signInWithPopup,
         GoogleAuthProvider
     } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-    // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
     // public face name = project-220732151957
-    // Your web app's Firebase configuration
     const firebaseConfig = {
         apiKey: "AIzaSyD92LfhAdYknR28dubJcBU7i2H07lZmtoU",
         authDomain: "ssd-project-49111.firebaseapp.com",
@@ -44,11 +41,11 @@
                         headers: {
                             'Content-Type': 'application/x-www-form-urlencoded'
                         },
-                        body: `google_userid=${encodeURIComponent(googleUserId)}&google_name=${encodeURIComponent(googleName)}`
+                        body: `google_userid=${encodeURIComponent(googleUserId)}&google_name=${encodeURIComponent(googleName)}&csrf_token=<?php echo $_SESSION['csrf_token']; ?>`
                     })
                     .then(response => response.text())
                     .then(data => {
-                        // Handle successful response, if necessary
+                        // Handle successful response
                         window.location.href = 'index.php'; // Redirect after successful login
                     })
                     .catch(error => {
